d4d80a6e4ace68de30e98164bf1ee9e570b672a4
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-05 09:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,22 +8,15 @@ Car.prototype.getMakeModel = function(){
 	return `${this.make} ${this.model}`;
 }
 
-// Create a new SportsCar instance
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-
-// Call getMakeModel() to get the make and model of the car
-console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-
-// Call getTopSpeed() to get the top speed of the sports car
-console.log(car.getTopSpeed()); // Output: 200
-
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
 	this.topSpeed = topSpeed;
 }
 
 SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = function(){
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function(){
 	return this.topSpeed;
 }
 
